5cbb56705c8c389a62afe9481ed8830d972f3195
mengpingdong <1251846863@qq.com>
2014-07-20 16:58:38 +0800

    first commit

---
+++ src/main/java/com/mpd/test/hellompd/App.java
@@ -0,0 +1,13 @@
+package com.mpd.test.hellompd;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
